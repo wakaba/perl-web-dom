d3a456c05c1eecd46d6bb4d63a8ce913f2671e6b
Wakaba <wakaba@lemon>
2013-01-08 22:25:30 +0900

    Added the remaining HTML element interfaces

--- lib/Web/DOM/HTMLElement.pm
+++ lib/Web/DOM/HTMLElement.pm
@@ -109,6 +109,328 @@ package Web::DOM::HTMLDivElement;
 our $VERSION = '1.0';
 push our @ISA, qw(Web::DOM::HTMLElement);
 
+package Web::DOM::HTMLAnchorElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLDataElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLTimeElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLSpanElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLBRElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+package Web::DOM::HTMLModElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLImageElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLIFrameElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLEmbedElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+# XXX plugin-specific interface
+
+package Web::DOM::HTMLObjectElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+# XXX plugin-specific interface
+
+package Web::DOM::HTMLParamElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLMediaElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLVideoElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLMediaElement);
+
+# XXX
+
+package Web::DOM::HTMLAudioElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLMediaElement);
+
+# XXX constructor
+
+package Web::DOM::HTMLSourceElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTrackElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLCanvasElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLMapElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLAreaElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTableElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTableCaptionElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+package Web::DOM::HTMLTableColElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLTableSectionElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLTableRowElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX
+
+package Web::DOM::HTMLTableCellElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTableDataCellElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLTableCellElement);
+
+package Web::DOM::HTMLTableHeaderCellElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLTableCellElement);
+
+# XXX 
+
+package Web::DOM::HTMLFormElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLFieldSetElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLLegendElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLLabelElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLInputElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLButtonElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLSelectElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLDataListElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLOptGroupElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLOptionElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTextAreaElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLKeygenElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLOutputElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLProgressElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLMeterElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLDetailsElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLMenuElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLMenuItemElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLDialogElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLAppletElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLMarqueeElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLFrameSetElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLFrameElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLBaseFontElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLDirectoryElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLFontElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
+package Web::DOM::HTMLTemplateElement;
+our $VERSION = '1.0';
+push our @ISA, qw(Web::DOM::HTMLElement);
+
+# XXX 
+
 1;
 
 =head1 LICENSE
